51362f54bbc5311a3dc05398e9da33c8dc1760bb
Andrew Miller <andrew.r.miller45@gmail.com>
2022-08-25 16:43:24 -0700

    add Modal class

---
+++ src/Modal.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Movies from "./Movies";
+import Card from "./Card"
+import './Modal.css'
+
+const Modal = (props) => {
+    console.log(props)
+}
+
+export default Modal
