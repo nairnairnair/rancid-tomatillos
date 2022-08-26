a739e9f868086ffca7636e0f48b3721eb898c768
emmacintosh <emmammrussell@gmail.com>
2022-08-25 18:41:40 -0600

    Convert Modal to a class, add HTML scaffolding for movie info, refactor App class state.

--- src/Modal.js
+++ src/Modal.js
@@ -3,8 +3,26 @@ import Movies from "./Movies";
 import Card from "./Card"
 import './Modal.css'
 
-const Modal = (props) => {
-    console.log(props)
+class Modal extends React.Component{
+    constructor(){
+        super()
+    }
+    render(){
+        return (
+            <section className="modalInfo">
+            <img src=""/>
+            <h1 className="title">Title:</h1>
+            <p className="release-date">Release Date:</p>
+            <small className="runtime">Runtime:</small>
+            <h2 className="tagline">Tagline:</h2>
+            <p className="genre">Genre:</p> 
+            <p className="avg-rating">Average Rating:</p>
+            <article className="overview">Overview</article>
+            <small className="budget">Budget:</small>
+            <small className="revenue">Revenue:</small>
+        </section>
+        )
+    }
 }
 
 export default Modal
