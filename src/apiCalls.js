ac0c6b39c05babdee7201b299e16d177317c50f1
Andrew Miller <andrew.r.miller45@gmail.com>
2022-09-01 19:43:43 -0700

    place api calls into their own .js file

---
+++ src/apiCalls.js
@@ -0,0 +1,14 @@
+const apiCalls = { 
+  getData: (id) => {
+    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)  
+    .then(rsp => {
+      console.log(rsp)
+      if (!rsp.ok) {
+        throw new Error('Unable to access the server.  Please try again!')
+      } else {
+          return rsp.json()
+          }           
+      })
+  }
+}
+  export default apiCalls
