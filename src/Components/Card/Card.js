483836f27a60fde70a86557c6db99bbb8f36c705
emmacintosh <emmammrussell@gmail.com>
2022-09-04 20:16:26 -0600

    Add a hover effect to movie cards

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -6,7 +6,13 @@ const Card = ({id, poster_path, filterMovies, title}) => {
     return (
             <div className="card">
                 <Link to={`/movies/${id}`} className='id-link'>
-                <img id={id} src={poster_path} alt={`${title} poster`} key={id} onClick={event => filterMovies(event, id)}/>
+                <img 
+                    className="card-image"
+                    id={id}
+                    src={poster_path} 
+                    alt={`${title} poster`} 
+                    key={id} 
+                    onClick={event => filterMovies(event, id)}/>
                 </Link>
             </div>
     )
