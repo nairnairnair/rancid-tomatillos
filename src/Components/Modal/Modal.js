c3aa69a99c5bdb2153c9f06e8d50c9f94cd83038
emmacintosh <emmammrussell@gmail.com>
2022-09-04 20:03:56 -0600

    Add flexbox rules to Modal UI, completely restructure Modal HTML to accomodate

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -7,15 +7,25 @@ const Modal = ({props}) => {
     return (
             <section className="modal">
                 <img className="modal-image" src={`${props.backdropPath}`} alt={`${props.title} poster`}/>
-                <h1 className="title">{props.title}</h1>
-                <p className="release-date">Release Date: {props.releaseDate}</p>
-                <small className="runtime">Runtime: {props.runtime} minutes</small>
-                <h2 className="tagline">{props.tagline}</h2> 
-                <p className="genre">{props.genres}</p> 
-                <p className="avg-rating">Average Rating: {props.averageRating}</p>
+                <div className="top-modal">
+                    <h1 className="title">{props.title}</h1>
+                    <h2 className="tagline">{props.tagline}</h2>
+                </div>
+                <section className="mid-modal">
+                    <div className="left-modal">
+                        <p className="release-date">Release Date: {props.releaseDate}</p>
+                        <p className="runtime">Runtime: {props.runtime} minutes</p>
+                    </div>
+                    <div className="right-modal">
+                        <p className="genre">{props.genres}</p>
+                        <p className="avg-rating">Average Rating: {props.averageRating}</p>
+                    </div>
+                </section>  
                 <article className="overview">{props.overview}</article>
-                <small className="budget">Budget: {props.budget}</small>
-                <small className="revenue">Revenue: {props.revenue}</small>
+                <div className="bottom-modal">
+                    <small className="budget">Budget: {props.budget}</small>
+                    <small className="revenue">Revenue: {props.revenue}</small>
+                </div>
                 <Link to={`/`} className='back-btn'><button className="close-button" onClick={() => {
                 }}>X</button></Link>
             </section>
